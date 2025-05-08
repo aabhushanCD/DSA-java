2cfa00e1400d823389c2ccb8337d49ed58b85665
aabhushanCD <dammardhakal426@gmail.com>
2025-05-08 22:37:25 +0545

    Added Quick Sort

---
+++ Sorting/QuickSort/tempCodeRunnerFile.java
@@ -0,0 +1,44 @@
+
+public class QuickSort {
+
+  public int[] Qsort(int[] array){
+    quickSort(array, 0, array.length-1);
+    return array;
+  }
+  private void quickSort(int[] array, int low, int high){
+    if(low<high){
+      int pivotIndex = partition(array, low, high);
+        quickSort(array, low, pivotIndex-1);
+        quickSort(array, pivotIndex+1, high);
+
+      }
+    }
+    private int partition(int[] array, int low, int high){
+      int pivot = array[high];
+      int i = low-1;
+
+      for(int j = low; j<high; j++){
+        if(array[j]<= pivot){
+          i++;
+          int temp = array[i];
+          array[i]= array[j];
+          array[j] = temp;
+        }
+      }
+
+      int temp = array[i + 1];
+      array[i+1] = array[high];
+      array[high] = temp;
+      return i+1;
+    }
+  
+  public static void main(String[] args) {
+    QuickSort sorter = new QuickSort();
+    int[] array = {9,4,3,677,3,643};
+    int[] sortedArray =sorter.Qsort(array);
+    for(int num : sortedArray){
+      System.out.print(num + " ");
+    }
+  }
+
+}
