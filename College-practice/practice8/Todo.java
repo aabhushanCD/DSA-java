2feaf814e73ec9dbba8474ccb9f4efc63cd8b7c0
aabhushanCD <dammardhakal426@gmail.com>
2025-07-11 20:05:20 +0545

    A simple todo

---
+++ College-practice/practice8/Todo.java
@@ -0,0 +1,76 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Todo extends JFrame implements ActionListener {
+
+    JButton Add, Delete;
+    JTextField text;
+    JTextArea textArea;
+    GridBagConstraints c = new GridBagConstraints();
+
+    public Todo() {
+
+        setLayout(new GridBagLayout());
+        setSize(1080, 720);
+
+        text = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+
+        add(text, c);
+
+        Add = new JButton("Add");
+        Add.addActionListener(this);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        add(Add, c);
+
+        textArea = new JTextArea();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+        add(textArea, c);
+
+        Delete = new JButton("Delete");
+        Delete.addActionListener(this);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 0;
+
+        add(Delete, c);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new GridLayout(3, 1));
+
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == Add) {
+            String input = text.getText();
+
+            if (!input.isEmpty()) {
+                textArea.append(" - " + input + "\n");
+                text.setText("");
+            }
+        }
+        if (event.getSource() == Delete) {
+            textArea.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Todo();
+    }
+}
